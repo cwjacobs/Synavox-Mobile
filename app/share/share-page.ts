bb1857edc01842229b3479d7eb61e0562982963a
Craig Jacobs <cjacobs@mimeo.com>
2019-03-31 17:28:59 -0400

    added new files to repository

---
+++ app/share/share-page.ts
@@ -0,0 +1,36 @@
+import { RadSideDrawer } from "nativescript-ui-sidedrawer";
+import * as app from "tns-core-modules/application";
+import { EventData } from "tns-core-modules/data/observable";
+import { NavigatedData, Page } from "tns-core-modules/ui/page";
+
+import { MedicineBinding } from "../data-models/medicine-binding";
+
+import { ShareViewModel } from "./share-view-model";
+import * as Test from "../data-models/test-data";
+
+let viewModel: ShareViewModel = null;
+
+export function onNavigatingTo(args: NavigatedData) {
+    const page = <Page>args.object;
+    viewModel = new ShareViewModel();
+    page.bindingContext = viewModel;
+}
+
+export function onDrawerButtonTap(args: EventData) {
+    const sideDrawer = <RadSideDrawer>app.getRootView();
+    sideDrawer.showDrawer();
+}
+
+export function onLoaded(args: EventData) {
+    viewModel.set("myMedicineList", Test.Dataset.getCurrentTestData());
+}
+
+export function onSelect(args: EventData) {
+    alert("onSelect");
+}
+
+export function onSearchTapped(args: EventData) {
+    alert("onSearchTapped");
+}
+
+
