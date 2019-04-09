1587b55021a5cc42509b1c403f3f3518bfa03a48
Craig Jacobs <cjacobs@mimeo.com>
2019-04-09 10:28:37 -0400

    added dose page, will merge and create new branch now

--- app/pair/pair-view-model.ts
+++ app/pair/pair-view-model.ts
@@ -1,7 +1,6 @@
 import { Observable } from "tns-core-modules/data/observable";
 
 import { SelectedPageService } from "../shared/selected-page-service";
-// import { Page } from "tns-core-modules/ui/page/page";
 
 export class PairViewModel extends Observable {
     constructor() {
